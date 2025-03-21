4da2a22e28b87f7f39658921d49d57610b29c269
hunainsheikh74 <pubglegend004@gmail.com>
2025-03-21 14:54:49 +0500

    Add files via upload

--- player.js
+++ player.js
@@ -20,6 +20,7 @@ const linkBtn = document.getElementById('linkbtn');
 const linkInput = document.getElementById('link');
 const video = document.getElementById('video');
 
+
 const minWidth = window.innerWidth;
 console.log(minWidth)
 if (minWidth > 1000) {
@@ -93,12 +94,38 @@ user = 'admin'
 linksRef.on('value', (dataSnapshot) => {
   const linkValue = dataSnapshot.val();
   video.src = linkValue;
+  video.currentTime = 1
 });
 
 video.addEventListener('timeupdate', (event) => {
+  
+  const currentTime = video.currentTime;
+  ready = 1
+const buffered = video.buffered;
+  for (let i = 0; i < buffered.length; i++) {
+    if (currentTime >= buffered.start(i) && currentTime <= buffered.end(i)) {
+      console.log('ready')
+      db.ref('links').child(user).update({
+        load: 'ready',
+        state: video.readyState
+      
+     })
+     ready = 0
+     
+      break;
+    } 
+      
     
     
-    const currentTime = video.currentTime;
+  }
+  if (ready === 1) {
+    db.ref('links').child(user).update({
+      load: 'loading',
+      state: video.readyState
+    
+   })
+  }
+    
     db.ref('links').child(user).update({
         time: currentTime,
         status : video.paused,
@@ -111,7 +138,7 @@ video.addEventListener('timeupdate', (event) => {
         if (user === 'guest') {
         const ctime = linkValue - video.currentTime;
         const post =  Math.abs(ctime);
-        if (post > 3){
+        if (post > 5){
             video.currentTime = linkValue
         }
     }
@@ -122,9 +149,10 @@ video.addEventListener('timeupdate', (event) => {
        
         const ctime = linkValue - video.currentTime;
         const post =  Math.abs(ctime);
-        if (post > 5){
+        if (post > 3){
             video.pause();
         }
+        
    })});
 
   video.addEventListener('waiting', () => {
@@ -137,14 +165,7 @@ video.addEventListener('timeupdate', (event) => {
       })
   });
   
-  video.addEventListener('canplay', () => {
-    
-    db.ref('links').child(user).update({
-        load: 'ready',
-        state: video.readyState
-       
-     })
-  });
+  
 
   video.addEventListener('stalled', () => {
     video.pause();
@@ -176,8 +197,10 @@ video.addEventListener('timeupdate', (event) => {
             video.pause();
         }
       });
-
+      
+    
   });
+ 
 
 
 const a1 = document.getElementById('a1');
@@ -221,8 +244,74 @@ const g2 = document.getElementById('g2');
     }
   });
 
-  const fullscreenbtn = document.getElementById('btnn');
-  fullscreenbtn.addEventListener('click', () => {
+  
+  video.addEventListener('click', () => {
     video.requestFullscreen();
   });
-  
+
+  const messageInput = document.getElementById('message');
+const scrollContainer = document.getElementById('chat-container');
+
+
+
+messageInput.addEventListener('focus', () => {
+  scrollContainer.scrollTop = scrollContainer.scrollHeight;
+});
+ 
+window.addEventListener('resize', () => {
+  scrollContainer.scrollTop = scrollContainer.scrollHeight;
+ } )
+
+ const inputField = document.getElementById('message');
+const button = document.getElementById('send');
+
+
+
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      const message = inputField.value
+      db.ref('links').child(user).update({
+            message: message
+        
+      }).then(inputField.value = '')
+    }
+  });
+
+  const container = document.getElementById('chat-container');
+  const label = document.createElement('label');
+
+  db.ref('links').child('guest').child('message').on('value', (snapshot) => {
+    
+    const value = snapshot.val();
+    if (user === 'guest') {
+      const label = document.createElement('label');
+      label.className = 'chat sent';
+      label.textContent = value;
+      container.appendChild(label);
+      container.scrollTop = scrollContainer.scrollHeight;
+    } else {
+      const label = document.createElement('label');
+      label.className = 'chat received';
+      label.textContent = value;
+      container.appendChild(label);
+      container.scrollTop = scrollContainer.scrollHeight;
+    }
+  });
+
+  db.ref('links').child('admin').child('message').on('value', (snapshot) => {
+   
+    const value = snapshot.val();
+    if (user === 'admin') {
+      const label = document.createElement('label');
+      label.className = 'chat sent';
+      label.textContent = value;
+      container.appendChild(label);
+      container.scrollTop = scrollContainer.scrollHeight;
+    } else {
+      const label = document.createElement('label');
+      label.className = 'chat received';
+      label.textContent = value;
+      container.appendChild(label);
+      container.scrollTop = scrollContainer.scrollHeight;
+    }
+  });
